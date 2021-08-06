ffbde5ba05a324de697d09e2f152548df7e9e72c
Sean Michael Shea <seanshea1337@gmail.com>
2021-08-05 20:55:14 -0400

    add call to action component as a test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import CallToAction from "./component/CallToAction/CallToAction";
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<CallToAction />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/component/CallToAction/CallToAction.js
@@ -0,0 +1,16 @@
+const CallToAction = () => {
+	return (
+		<>
+			<h1> Welcome to the Love Poem Calculator</h1>
+			<p>
+				Are you ever unsure whether the person you are with or seeking
+				to date is the one? Keeping tabs on your love life seems hard,
+				but you don't want to become too dependent on something so
+				unreliable as your zodiac sign
+			</p>
+			<p>If this describes you, then you're in luck!</p>
+		</>
+	);
+};
+
+export default CallToAction;
