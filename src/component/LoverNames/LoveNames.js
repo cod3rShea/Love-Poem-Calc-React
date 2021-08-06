05c10b3e1c07af46e60a2a723bc8a59b3376f864
Sean Michael Shea <seanshea1337@gmail.com>
2021-08-05 21:19:58 -0400

    create useState for your name and crush name. Setup input to store and update names

--- src/component/LoverNames/LoveNames.js
+++ src/component/LoverNames/LoveNames.js
@@ -1,23 +1,25 @@
+import React, { useState } from "react";
 const LoverNames = () => {
+	const [yourName, setYourName] = useState("");
+	const [yourCrushName, setYourCrushName] = useState("");
+
 	return (
 		<>
 			<h2>Enter Two Names:</h2>
 			<input
-				id="f-name"
-				type="text"
-				class="name"
-				spellcheck="false"
+				name="yourName"
+				value={yourName}
 				placeholder="Your Name"
+				onChange={(e) => setYourName(e.target.value)}
 			/>
 			<input
-				id="l-name"
-				type="text"
-				class="name"
-				spellcheck="false"
-				placeholder="Your crush"
+				name="yourName"
+				value={yourCrushName}
+				placeholder="Your Crush's Name"
+				onChange={(e) => setYourCrushName(e.target.value)}
 			/>
 			<input
-				class="btn btn-primary"
+				className="btn btn-primary"
 				id="go"
 				type="submit"
 				value="Get Poem"
