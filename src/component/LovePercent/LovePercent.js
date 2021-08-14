0d8d2f4bc547a672c8b0978acdd32e3f96be32f2
Sean Michael Shea <seanshea1337@gmail.com>
2021-08-14 09:47:47 -0400

    remove unused useRef

--- src/component/LovePercent/LovePercent.js
+++ src/component/LovePercent/LovePercent.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import LovePercentContent from "../LovePercentContent/LovePercentContent";
 import LovePoem from "../LovePoem/LovePoem";
 
